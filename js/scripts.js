b2154d63ab602d598d5acd89c7bf4d7f753f9713
ToasterTom <tate.thomas5595@gmail.com>
2017-01-05 22:31:24 -0700

    hamburger set, site responsive, porfolio squares aligned

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+// Hamburger Function
+$('.hamburger').on('click', function(e) {
+  // Prevent link from jumping to the top of the page
+  e.preventDefault();
+  // If menu is already showing, slide it up. Otherwise, slide it down.
+  $('.menu').toggleClass('slide-down');
+});
